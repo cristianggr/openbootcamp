083bdb5bab24e258e5ecba147f6a2d4d367994b4
Cristian Guillén <60959272+cristianggr@users.noreply.github.com>
2022-07-16 21:30:49 +0200

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        double num1 = 2.30;
+        double num2 = 3.25;
+        double num3 = 6.71;
+        sumar(num1, num2, num3);
+
+    }
+    public static void sumar(double a, double b, double c) {
+        double suma = a + b + c;
+        System.out.println(suma);
+    }
+}
